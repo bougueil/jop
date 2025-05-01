b72fbfc82f954eca3d6339b3f42a761e3a05579c
bougueil <rmariana@gmail.com>
2025-05-01 10:40:13 +0200

    update deps

--- mix.exs
+++ mix.exs
@@ -6,34 +6,29 @@ defmodule Jop.Mixfile do
     [
       app: :jop,
       version: @version,
-      elixir: ">= 1.14.1",
+      elixir: "~> 1.15-dev",
       package: package(),
       start_permanent: Mix.env() == :prod,
       description: "an in-memory loggger for spatial / temporal search",
       deps: deps(),
       docs: [
-        main: "Superls",
+        main: "Jop",
         source_ref: "v#{@version}",
-        source_url: "https://github.com/bougueil/superls"
+        source_url: "https://github.com/bougueil/jop"
       ]
     ]
   end
 
   def application do
-    [
-      extra_applications: [
-        :logger
-        # , :os_mon
-      ]
-    ]
+    [extra_applications: [:logger]]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.30", only: [:docs, :test], runtime: false},
-      {:dialyxir, "~> 1.2", only: [:dev, :test], runtime: false},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:ex_doc, "~> 0.37", only: [:docs, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
